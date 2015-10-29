7081df08402507e0e56ffe89427550957a3a3e1c
rogeralbinoi <roger.albino.i@gmail.com>
2015-10-28 22:26:49 -0200

    Adicionando estrutura html5 para o calendário php

--- back-end/php/calendario/calendario.php
+++ back-end/php/calendario/calendario.php
@@ -1,11 +1,20 @@
-<table border="1">
-  <tr>
-    <th>Dom</th>
-    <th>Seg</th>
-    <th>Ter</th>
-    <th>Qua</th>
-    <th>Qui</th>
-    <th>Sex</th>
-    <th>Sab</th>
-  </tr>
-</table>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Calendário php</title>
+    <meta charset="urf-8">
+  </head>
+  <body lang="pt-br">
+    <table border="1">
+      <tr>
+        <th>Dom</th>
+        <th>Seg</th>
+        <th>Ter</th>
+        <th>Qua</th>
+        <th>Qui</th>
+        <th>Sex</th>
+        <th>Sab</th>
+      </tr>
+    </table>
+  </body>
+</html>
