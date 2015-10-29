88a03b25303a8739c1393dcb4a0486482a4fa05b
rogeralbinoi <roger.albino.i@gmail.com>
2015-10-28 22:38:04 -0200

    Adicionando Titulo e centralizando calendário php

--- back-end/php/calendario/calendario.php
+++ back-end/php/calendario/calendario.php
@@ -4,8 +4,14 @@
     <title>Calendário php</title>
     <meta charset="urf-8">
     <style>
+      h1 {
+        font-size: 3em;
+        text-align: center;
+        color: #795548;
+      }
       table {
         padding: .2em;
+        margin: 0 auto;
         background: #EFEBE9;
       }
       th {
@@ -16,6 +22,7 @@
     </style>
   </head>
   <body lang="pt-br">
+    <h1><?php echo "Calendário php" ?></h1>
     <table>
       <tr>
         <th>Dom</th>
